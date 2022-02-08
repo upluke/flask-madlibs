d9cbdb9d5b83d6491770d5fb84a6eec0656d05c5
upluke <lukeforadream@gmail.com>
2022-02-08 00:33:25 -0800

    selected value

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from copyreg import constructor
 from stories import Story
 from modulefinder import packagePathMap
 from flask import Flask, request, render_template
@@ -6,11 +7,18 @@ import sys
 app = Flask(__name__)
 sys.path.append(".")
 
+templates = ["""Once upon a time in a long-ago {place}, there lived a
+       large {adjective} {noun}. It loved to {verb} {plural_noun}.""",
+             """Twice upon a time in a long-ago {place}, there lived a
+       large {adjective} {noun}. It loved to {verb} {plural_noun}.""",
+             """Three upon a time in a long-ago {place}, there lived a
+       large {adjective} {noun}. It loved to {verb} {plural_noun}."""]
+
 
 @app.route('/')
 def index():
 
-    return render_template('home.html')
+    return render_template('home.html', templates=templates)
 
 
 @app.route('/story')
@@ -20,5 +28,8 @@ def compose_story():
     verb = request.args["verb"]
     adjective = request.args["adjective"]
     plural_noun = request.args["plural_noun"]
-    print(Story)
-    return render_template('story.html', place=place, noun=noun, verb=verb, adjective=adjective, plural_noun=plural_noun)
+    print(request.args)
+    selected_temp = request.args["template"]
+    # print(selected_temp)
+
+    return render_template('story.html', place=place, noun=noun, verb=verb, adjective=adjective, plural_noun=plural_noun, selected_temp=selected_temp)
